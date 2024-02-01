e5b7a2940482c9b2acee04a2790e6b5986e3b445
Michael <michaelsmike7@yahoo.com>
2024-02-01 09:32:31 -0500

    added more test cases

--- test_case.py
+++ test_case.py
@@ -9,11 +9,17 @@ class MyTestCase(unittest.TestCase):
     def test_check_straight_high(self):
         self.assertEqual(practice.check_straight("S7", "S8", "S9"), 9)
 
+    def test_check_straight_zero(self):
+        self.assertEqual(practice.check_straight("S7", "S8", "S2"), 0)
+
+
     def test_check_3ofa_kind(self):
         self.assertEqual(practice.check_3ofa_kind("S7", "S7", "S7"), 7)
 
     def test_check_3ofa_kind_Ace(self):
         self.assertEqual(practice.check_3ofa_kind("SA", "SA", "SA"), 14)
+    def test_check_3ofa_kind_zero(self):
+        self.assertEqual(practice.check_3ofa_kind("SA", "SA", "S3"), 0)
 
     def test_check_royal_flush(self):
         self.assertEqual(practice.check_royal_flush("S10", "SJ", "SQ"), 12)
